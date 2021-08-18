a936b0867f5b6ea174f0df153ea6f74204506eb9
ImGyo <jywlsh3@naver.com>
2021-08-18 17:30:07 +0900

    AndroidBridge 세팅

---
+++ app/src/main/java/com/example/greeners_aos/AndroidBridge.java
@@ -0,0 +1,36 @@
+package com.example.greeners_aos;
+
+import android.os.Handler;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+
+public class AndroidBridge {
+
+    private String TAG = "AndroidBridge";
+    final public Handler handler = new Handler();
+
+    // 생성 시 내부적으로 사용할 코드들 저장
+    private WebView mAppView;
+    private MainActivity mContext;
+
+    // 기본 생성자
+    public AndroidBridge(WebView _mAppView, MainActivity _mContext){
+        mAppView = _mAppView;
+        mContext = _mContext;
+    }
+
+    // 로그 메소드
+    @JavascriptInterface
+    public void call_log(final String _message){
+        Log.d(TAG, _message);
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                mAppView.loadUrl("javascript:alert('"+_message+"라는 로그발생')");
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/example/greeners_aos/MainActivity.java
+++ app/src/main/java/com/example/greeners_aos/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.greeners_aos;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    // 웹뷰 url 세팅
+    private final String webViewUrl = "https://www.naver.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 웹뷰 컨트롤러 가져옴
+        mWebView = findViewById(R.id.webview);
+
+        // 상단 툴바 제거
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        mWebView.getSettings().setUseWideViewPort(true); // wide viewport를 사용하도록 설정
+        mWebView.getSettings().setJavaScriptEnabled(true); // 자바스크립트 사용 허용
+        mWebView.getSettings().setLoadWithOverviewMode(true); // 웹 컨텐츠가 웹뷰보다 클 경우 스크린 크기에 맞게 조정
+
+        mWebView.setWebViewClient(new WebViewClient(){}); // 웹뷰를 웹뷰레이아웃 내에서 사용하기 위해 설정
+
+        // 웹뷰로 보여줄 url주소 세팅
+        mWebView.loadUrl(webViewUrl);
+
+        AndroidBridge androidBridge = new AndroidBridge(mWebView, MainActivity.this);
+        mWebView.addJavascriptInterface(androidBridge, "Android");
     }
 }
